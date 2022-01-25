96253a3b235595b37ad95668952a63665a86b657
Alexandru Adrian Vinulescu <AAAVU@vestas.com>
2022-01-25 17:10:47 +0100

    UI updates

--- public/Dashboard/testAPI.js
+++ public/Dashboard/testAPI.js
@@ -1,25 +1,5 @@
-var data = {
-    resource_id: 'co2emis', // the resource id
-    limit: 5, // get 5 results
-    sort: "Minutes5DK desc"
-  };
-  $.ajax({
-    url: 'https://api.energidataservice.dk/datastore_search',
-    type: "GET",
-    data: data,
-    dataType: 'JSON',
-    help: "https://api.energidataservice.dk/datastore_search",
-    success: function(result) {
-      
-      for (let i = 0; i < result.result.records.length; i++) {
-        console.log('Total results found: ' + result.result.records[i]['CO2Emission'], "@", result.result.records[i]['Minutes5DK']);
- 
-      }
-      
-    }
-  });
-
-
-
-
-
+var today = new Date();
+var date = today.getDate() +'-'+(today.getMonth()+1) +'-'+ today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+  document.getElementById("currentDateTime").innerHTML = dateTime;
